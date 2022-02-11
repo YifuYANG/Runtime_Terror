42d3423c7f8f8e34fadf5e72075a60ce2b7d7625
yifuyang <yifu.yang@ucdconnect.ie>
2022-02-11 19:25:59 +0000

    csrf perfection enabled

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -21,6 +21,8 @@ function Login(){
                 }
             }
         }
+        //set csrf header
+        xhr.setRequestHeader("X-XSRF-TOKEN",getCookie("XSRF-TOKEN"))
         xhr.send(JSON.stringify(info));
     } else {
         alert("Invalid email address! please try again");
@@ -31,4 +33,10 @@ function Login(){
 
 function Redirect(){
     window.location.href = "/register";
+}
+//function to get cookie from frontend
+function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(';').shift();
 }
