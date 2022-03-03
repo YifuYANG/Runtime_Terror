cdcb6875455ebc2b72f4a46c2df1e129b9c471ea
Mingqi948 <yangmingqi1001@sina.com>
2022-03-03 12:29:45 +0000

    fix activity page

--- src/main/java/app/dao/ActivityDao.java
+++ src/main/java/app/dao/ActivityDao.java
@@ -24,19 +24,53 @@ public class ActivityDao {
         List<ActivityForm> results = new ArrayList<>();
         for(Appointment a : appointments) {
             ActivityForm activity = new ActivityForm();
-            if(a.getDose_1_center()!=null){
-            activity.setFirst_dose_center(a.getDose_1_center().name());
+
+            if(a.getDose_1_center() != null) {
+                activity.setFirst_dose_center(a.getDose_1_center().name());
+            } else {
+                activity.setFirst_dose_center("NA");
             }
-            activity.setFirst_dose_date(a.getDose_1_date());
-            if(a.getDose_1_status()!=null){
-            activity.setFirst_dose_status(a.getDose_1_status().name());
+
+            if(a.getDose_1_status() != null) {
+                activity.setFirst_dose_status(a.getDose_1_status().name());
+            } else {
+                activity.setFirst_dose_status("NA");
             }
-            if(a.getDose_2_center()!=null){
-            activity.setSecond_dose_center(a.getDose_2_center().name());
+
+            if(a.getDose_1_date() != null) {
+                activity.setFirst_dose_date(a.getDose_1_date().toString());
+            } else {
+                activity.setFirst_dose_date("NA");
             }
-            activity.setSecond_dose_date(a.getDose_2_date());
-            if(a.getDose_2_status()!=null){
-            activity.setSecond_dose_status(a.getDose_2_status().name());
+
+            if(a.getDose_1_Slot() != null) {
+                activity.setFirst_dose_time(a.getDose_1_Slot().toString());
+            } else {
+                activity.setFirst_dose_time("NA");
+            }
+
+            if(a.getDose_2_center() != null) {
+                activity.setSecond_dose_center(a.getDose_2_center().name());
+            } else {
+                activity.setSecond_dose_center("NA");
+            }
+
+            if(a.getDose_2_status() != null) {
+                activity.setSecond_dose_status(a.getDose_2_status().name());
+            } else {
+                activity.setSecond_dose_status("NA");
+            }
+
+            if(a.getDose_2_date() != null) {
+                activity.setSecond_dose_date(a.getDose_2_date().toString());
+            } else {
+                activity.setSecond_dose_date("NA");
+            }
+
+            if(a.getDose_2_slot() != null) {
+                activity.setSecond_dose_time(a.getDose_2_slot().toString());
+            } else {
+                activity.setSecond_dose_time("NA");
             }
             results.add(activity);
         }
--- src/main/java/app/vo/ActivityForm.java
+++ src/main/java/app/vo/ActivityForm.java
@@ -5,17 +5,18 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.Date;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class ActivityForm {
     private String first_dose_center;
-    private Date first_dose_date;
+    private String first_dose_date;
+    private String first_dose_time;
+    private String second_dose_time;
     private String first_dose_status;
     private String second_dose_center;
-    private Date second_dose_date;
+    private String second_dose_date;
     private String second_dose_status;
 }
 
